f1df6b8e38f12b5bec05814ef27a2161c1dc347e
SahilThakur7 <sahilrd07@gmail.com>
2017-12-26 14:02:28 +0530

    Added state via input

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            deadline: 'January 1, 2018',
+            newDeadline: ''
+        }
+
+    }
+
+    changeDeadline(){
+        this.setState({deadline: this.state.newDeadline})
+    }
     render(){
         return(
             <div className="App">
-                <div className="App-title">Countdown to New Year</div>
+                <div className="App-title">Countdown to New Year({this.state.deadline})</div>
                 <div>
                     <div className="Clock-days">days</div>
                     <div className="Clock-hrs">hrs</div>
@@ -13,10 +25,10 @@ class App extends React.Component {
                     <div className="Clock-secs">secs</div>
                 </div>
                 <div>
-                    <input placeholder="new date" />
-                    <button>Submit</button>
+                    <input placeholder="new date" onChange={(event)=>this.setState({newDeadline: event.target.value})} />
+                    <button onClick={ () => this.changeDeadline()}>Submit</button>
                 </div>
-            </div>
+        </div>
         );
     }
 }
